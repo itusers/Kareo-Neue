372770701cd44312db1bff7208a3b503d25095c0
Abe Yang <abeyang@gmail.com>
2013-03-14 11:19:22 -0700

    Added Sandbox/template.html

--- Chrome/_footer.php
+++ Chrome/_footer.php
@@ -1,9 +1,10 @@
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<!--         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
         <script src="../Assets/bootstrap/js/bootstrap-transition.js"></script>
         <script src="../Assets/bootstrap/js/bootstrap-modal.js"></script>
-        
+         -->
+		 
         <!-- Typekit -->
         <script type="text/javascript" src="//use.typekit.net/usb3bya.js"></script>
         <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
