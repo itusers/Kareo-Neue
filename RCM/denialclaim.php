5927ba3e917718f6a7e1cbd95761aac1fe4215a3
Abe Yang <abeyang@gmail.com>
2013-08-12 16:14:25 -0700

    Layout for multiple encounters
    Big change

--- RCM/denialclaim.php
+++ RCM/denialclaim.php
@@ -9,7 +9,7 @@
 	<div class="row">
 		<section class="col-8">	
 			<div ng-repeat="patient in patients">
-				<div ng-class="{current: $first }" class="patient card">
+				<div ng-class="{current: $first }" class="patient wrapper">
 					<div class="stamp stamp-appeal">
 						<i class="icon-bolt"></i> Appealed
 					</div>
@@ -28,171 +28,268 @@
 						<i class="icon-refresh"></i> Resubmitted
 					</div>
 
-					<div class="header pad row">
-						<h2 class="col-6"><i class="icon-{{patient.gender}}"></i> {{patient.first}} {{patient.last}} <i class="icon-pencil edit"></i></h2> 
-						<div class="col-3 smalltext">
-							DOB <strong>{{prettydate(patient.dob)}}</strong>
-							<br />SSN <strong>{{patient.ssn}}</strong>
+					<div class="panel bot-10 open">
+						<div class="panel-heading">
+							<h2 class="panel-title">
+								<i class="icon-pencil edit pull-right"></i>
+								<i class="icon-{{patient.gender}}"></i> {{patient.first}} {{patient.last}}
+							</h2>
 						</div>
-						<div class="col-3 smalltext">
-							Policy # <strong>{{patient.policy}}</strong>
-							<br />Group # <strong>{{patient.group}}</strong>
-						</div>
-					</div> <!-- /.row -->
-
-					<div class="pad">
-						<div class="alert alert-warning">
-							<strong>15 days left</strong> until estimated filing deadline 
-						</div>
-						<div class="lifecycle">
-							<!-- DOS -->
-							<i class="icon-circle text-muted" style="left: 0%;" data-toggle="tooltip" title="<strong>Date of Service</strong> {{prettydate(patient.dos)}}"></i>
-							<!-- Estimated filing deadline -->
-							<i class="icon-frown text-danger" style="left: 98%;" data-toggle="tooltip" title="<strong>Filing Deadline</strong> {{deadlinefromdate(patient.dos)}}"></i>
-							<!-- TODAY -->
-							<i class="icon-map-marker text-success icon-2x" style="left: 83%;" data-toggle="tooltip" title="<strong>TODAY</strong>"></i>
-
-							<!-- Claim submissions -->
-							<i class="icon-circle-blank text-primary" style="left: 18%;" data-toggle="tooltip" title="<strong>Submission</strong> 8/16/2013"></i>
-							<i class="icon-circle-blank text-primary" style="left: 44%;" data-toggle="tooltip" title="<strong>Submission</strong> 9/19/2013"></i>
-							<i class="icon-circle-blank text-primary" style="left: 70%;" data-toggle="tooltip" title="<strong>Submission</strong> 10/7/2013"></i>
-							<!-- Claim rejections -->
-							<i class="icon-remove text-danger" style="left: 25%;" data-toggle="tooltip" title="<strong>Rejection</strong> 8/20/2013"></i>
-							<!-- Claim denials -->
-							<i class="icon-ban-circle text-danger" style="left: 75%;" data-toggle="tooltip" title="<strong>Denial</strong> 10/11/2013"></i>
-		
-							<div class="progress progress-striped">
-								<div class="progress-bar progress-bar-warning" style="width: 25%"></div>
+						<div class="list-group-flush pad pad-left row">
+							<div class="col-6 smalltext">
+								DOB <strong>{{prettydate(patient.dob)}}</strong>
+								<br />SSN <strong>{{patient.ssn}}</strong>
 							</div>
-						</div> <!-- /.lifecycle -->
+							<div class="col-6 smalltext">
+								Policy # <strong>{{patient.policy}}</strong>
+								<br />Group # <strong>{{patient.group}}</strong>
+							</div>
+						</div>
 					</div>
 
-					<div class="pad claims">
-						<h3>Claims <i class="icon-pencil edit"></i></h3>
-						<table class="table table-hover">
-							<thead>
-							  <tr>
-							    <th>&nbsp;</th>
-							    <th>DOS</th>
-							    <th>Procedure</th>
-							    <th>Amount</th>
-							  </tr>
-							</thead>
-							<tbody>
-							  <tr>
-							    <td><i class="icon-ok text-success"></i></td>
-							    <td>{{prettydate(patient.dos)}}</td>
-							    <td>99214</td>
-							    <td>$136.00</td>
-							  </tr>
-							  <tr>
-							    <td><i class="icon-ban-circle text-danger"></i></td>
-							    <td>{{prettydate(patient.dos)}}</td>
-							    <td>36415</td>
-							    <td>$257.00</td>
-							  </tr>
-							  <tr>
-							    <td><i class="icon-ban-circle text-danger"></i></td>
-							    <td>{{prettydate(patient.dos)}}</td>
-							    <td>83036</td>
-							    <td>$312.00</td>
-							  </tr>
-							</tbody>
-						</table>
-					</div>
+					<div class="panel bot-10 open">
+						<div class="panel-heading">
+							<h2 class="panel-title">
+								<div class="pull-right info text-warning">
+									<strong>15 days left</strong> until estimated filing deadline 
+									<i class="icon-pencil edit"></i>
+								</div>
+								
+								<i class="icon-medkit"></i> Encounter #2507
+							</h2>
+						</div>
+						<div class="list-group-flush controls">
+							<div class="row">
+								<div class="col col-4 border">
+									<div class="descriptor">Assigned to</div>
+									<div class="btn-group">
+										<button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown">Abe Yang (Biller) <i class="icon-angle-down"></i></button>
+										<ul class="dropdown-menu">
+											<li><a href="#">Abe Yang (Biller)</a></li>
+											<li><a href="#">Mitch Malone (Biller)</a></li>
+											<li><a href="#">Patrick Bartenstein (Biller)</a></li>
+											<li><a href="#">Neil Chopra (Office Manager)</a></li>
+											<li><a href="#">Dr. Savan Kong (Provider)</a></li>
+											<li><a href="#">Dr. Randy Sinnott (Provider)</a></li>
+											<li><a href="#">Dr. Tom Giannulli (Provider)</a></li>
+										</ul>	
+									</div> <!-- /.btn-group -->
+								</div>
+								<div class="col col-8">
+									<div class="descriptor">Labels</div>
+									
+									<div class="btn-group">
+										<button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown">Awaiting EOB <i class="icon-angle-down"></i></button>
+										<ul class="dropdown-menu">
+											<li class="dropdown-header">Custom Status</li>
+											<li><a href="#">Awaiting EOB</a></li>
+											<li><a href="#">To Appeal</a></li>
+											<li><a href="#">Appealed</a></li>
+											<li><a href="#">Support Docs Needed</a></li>
+											<li><a href="#">None</a></li>
+										</ul>	
+									</div> <!-- /.btn-group -->
 
-					<div class="pad history">
-						<h3>Activity History</h3>
-						<div class="history-inner">
-							<div class="row">
-								<div class="col-2 date">{{prettydate(patient.dos)}}</div>
-								<div class="col-1 timeline"><i class="icon-circle text-muted"></i></div>
-								<div class="col-9">Date of service</div>
-							</div>
-							<div class="row">
-								<div class="col-2 date">8/16/2013</div>
-								<div class="col-1 timeline"><i class="icon-circle-blank text-primary"></i></div>
-								<div class="col-9">Claim submitted by Abe Yang</div>
-							</div>
-							<div class="row">
-								<div class="col-2 date text-danger">8/20/2013</div>
-								<div class="col-1 timeline"><i class="icon-remove text-danger"></i></div>
-								<div class="col-9 text-danger"><strong>Rejected</strong>. Aenean lacinia bibendum nulla sed onsectetur. Nullam id dolor id nibh ultricies vehicula ut id elit.</div>
-							</div>
-							<div class="row">
-								<div class="col-2 date">8/21/2013</div>
-								<div class="col-1 timeline"><i class="icon-comment"></i></div>
-								<div class="col-9"><strong>Jason Leu</strong>: Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
-							</div>
-							<div class="row">
-								<div class="col-2 date">8/21/2013</div>
-								<div class="col-1 timeline"><i class="icon-pencil"></i></div>
-								<div class="col-9">Edited claim line-item by Jason</div>
-							</div>
-							<div class="row">
-								<div class="col-2 date">9/19/2013</div>
-								<div class="col-1 timeline"><i class="icon-circle-blank text-primary"></i></div>
-								<div class="col-9">Claim submitted by Jason Leu</div>
-							</div>
-							<div class="row today">
-								<div class="col-2 date">Today</div>
-								<div class="col-1 timeline"><i class="icon-comment"></i></div>
-								<div class="col-9">
-									<textarea class="form-control" rows="2"></textarea>
-									<button type="button" class="btn btn-primary" disabled="disabled">Add comment</button>
+									<div class="btn-group">
+										<button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown"><span class="title">Importance</span> <i class="icon-angle-down"></i></button>
+										<ul class="dropdown-menu">
+											<li class="dropdown-header">Importance</li>
+											<li><a href="#">High</a></li>
+											<li><a href="#">Medium</a></li>
+											<li><a href="#">Low</a></li>
+											<li><a href="#">None</a></li>
+										</ul>	
+									</div> <!-- /.btn-group -->
 								</div>
 							</div>
-						</div> <!-- /.history-inner -->
-					</div>
-				</div> <!-- /.patient.card -->
+						</div>
+						<div class="list-group-flush pad">
+							<div class="lifecycle">
+								<!-- DOS -->
+								<i class="icon-circle text-muted" style="left: 0%;" data-toggle="tooltip" title="<strong>Date of Service</strong> {{prettydate(patient.dos)}}"></i>
+								<!-- Estimated filing deadline -->
+								<i class="icon-frown text-danger" style="left: 98%;" data-toggle="tooltip" title="<strong>Filing Deadline</strong> {{deadlinefromdate(patient.dos)}}"></i>
+								<!-- TODAY -->
+								<i class="icon-map-marker text-success icon-2x" style="left: 83%;" data-toggle="tooltip" title="<strong>TODAY</strong>"></i>
+
+								<!-- Claim submissions -->
+								<i class="icon-circle-blank text-primary" style="left: 18%;" data-toggle="tooltip" title="<strong>Submission</strong> 8/16/2013"></i>
+								<i class="icon-circle-blank text-primary" style="left: 44%;" data-toggle="tooltip" title="<strong>Submission</strong> 9/19/2013"></i>
+								<i class="icon-circle-blank text-primary" style="left: 70%;" data-toggle="tooltip" title="<strong>Submission</strong> 10/7/2013"></i>
+								<!-- Claim rejections -->
+								<i class="icon-remove text-danger" style="left: 25%;" data-toggle="tooltip" title="<strong>Rejection</strong> 8/20/2013"></i>
+								<!-- Claim denials -->
+								<i class="icon-ban-circle text-danger" style="left: 75%;" data-toggle="tooltip" title="<strong>Denial</strong> 10/11/2013"></i>
+			
+								<div class="progress progress-striped">
+									<div class="progress-bar progress-bar-warning" style="width: 25%"></div>
+								</div>
+							</div> <!-- /.lifecycle -->
+						</div>
+
+						<div class="list-group-flush pad claims">
+							<h3>Claims</h3>
+							<table class="table table-hover">
+								<thead>
+								  <tr>
+								    <th>&nbsp;</th>
+								    <th>DOS</th>
+								    <th>Procedure</th>
+								    <th>Amount</th>
+								  </tr>
+								</thead>
+								<tbody>
+								  <tr>
+								    <td><i class="icon-ok text-success"></i></td>
+								    <td>{{prettydate(patient.dos)}}</td>
+								    <td>99214</td>
+								    <td>$136.00</td>
+								  </tr>
+								  <tr>
+								    <td><i class="icon-ban-circle text-danger"></i></td>
+								    <td>{{prettydate(patient.dos)}}</td>
+								    <td>36415</td>
+								    <td>$257.00</td>
+								  </tr>
+								  <tr>
+								    <td><i class="icon-ban-circle text-danger"></i></td>
+								    <td>{{prettydate(patient.dos)}}</td>
+								    <td>83036</td>
+								    <td>$312.00</td>
+								  </tr>
+								</tbody>
+							</table>
+						</div>
+
+						<div class="list-group-flush pad history">
+							<h3>Activity History</h3>
+							<div class="history-inner">
+								<div class="row">
+									<div class="col-2 date">{{prettydate(patient.dos)}}</div>
+									<div class="col-1 timeline"><i class="icon-circle text-muted"></i></div>
+									<div class="col-9">Date of service</div>
+								</div>
+								<div class="row">
+									<div class="col-2 date">8/16/2013</div>
+									<div class="col-1 timeline"><i class="icon-circle-blank text-primary"></i></div>
+									<div class="col-9">Claim submitted by Abe Yang</div>
+								</div>
+								<div class="row">
+									<div class="col-2 date text-danger">8/20/2013</div>
+									<div class="col-1 timeline"><i class="icon-remove text-danger"></i></div>
+									<div class="col-9 text-danger"><strong>Rejected</strong>. Aenean lacinia bibendum nulla sed onsectetur. Nullam id dolor id nibh ultricies vehicula ut id elit.</div>
+								</div>
+								<div class="row">
+									<div class="col-2 date">8/21/2013</div>
+									<div class="col-1 timeline"><i class="icon-comment"></i></div>
+									<div class="col-9"><strong>Jason Leu</strong>: Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+								</div>
+								<div class="row">
+									<div class="col-2 date">8/21/2013</div>
+									<div class="col-1 timeline"><i class="icon-pencil"></i></div>
+									<div class="col-9">Edited claim line-item by Jason</div>
+								</div>
+								<div class="row">
+									<div class="col-2 date">9/19/2013</div>
+									<div class="col-1 timeline"><i class="icon-circle-blank text-primary"></i></div>
+									<div class="col-9">Claim submitted by Jason Leu</div>
+								</div>
+								<div class="row today">
+									<div class="col-2 date">Today</div>
+									<div class="col-1 timeline"><i class="icon-comment"></i></div>
+									<div class="col-9">
+										<textarea class="form-control" rows="2" placeholder="Write a comment"></textarea>
+									</div>
+								</div>
+							</div> <!-- /.history-inner -->
+
+						</div>
+						<div class="panel-footer">
+							<div class="btn-group pull-right">
+								<button type="button" class="btn btn-primary">Comment</button>
+							</div>
+							
+							<div class="btn-group">
+								<button type="button" class="btn btn-default">Appeal</button>	
+								<button type="button" class="btn btn-default">Settle</button>
+								<div class="btn-group">
+								    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+										Snooze
+										<i class="icon-angle-down"></i>
+								    </button>
+								    <ul class="dropdown-menu">
+										<li><a href="#">1 day</a></li>
+										<li><a href="#">2 days</a></li>
+										<li><a href="#">3 days</a></li>
+										<li><a href="#">4 days</a></li>
+										<li><a href="#">5 days</a></li>
+										<li><a href="#">10 days</a></li>
+										<li><a href="#">15 days</a></li>
+								    </ul>
+								</div>	
+							</div>
+						</div>
+					</div> <!-- /.panel -->
+
+				</div> <!-- /.patient.wrapper -->
 			</div>
 		</section>
 
 		<aside class="col-4">
-			<div class="card">
-				<div class="pad">
-					<div class="descriptor">Remark Code</div>
-					<h2>CO-140</h2>
-					<p>Patient/Insured health identification number and name do not match.</p>
+			<div class="panel panel-danger bot-10 open">
+				<div class="panel-heading">
+					<h3 class="panel-title"><i class="icon-exclamation-sign"></i> CO-140</h3>
 				</div>
-			</div>
-			<div class="card">
-				<div class="pad">
-					<div class="descriptor">Insurance</div>
-					<h2>Aetna Health</h2>
-					<h3>1-800-123-4456</h3>
-					<p>Estimated time for filing: 90 days</p>
+				<div class="list-group-flush pad pad-left">
+					<strong>CO-140</strong> Patient/Insured health identification number and name do not match.		
 				</div>
 			</div>
 
-			<div class="nocard">
-				<div class="meta-info">
-					<i class="icon-home main-icon"></i>
-					<p>
-						<h4>Davenport Medical Clinic <i class="icon-pencil edit"></i></h4>
-						Group NPI <strong>199457851</strong>
-						<br />EIN <strong>45-584562</strong>
-					</p>
+			<div class="panel bot-10">
+				<div class="panel-heading">
+					<h3 class="panel-title">
+						<span class="pull-right">1-800-123-4456 <i class="icon-pencil edit"></i></span>
+						<i class="icon-building"></i> Aetna Health
+					</h3>
 				</div>
-				<div class="meta-info">
-					<i class="icon-user-md main-icon"></i>
-					<p>
-						<h4>Randy Sinnott, DO <i class="icon-pencil edit"></i></h4>
-						NPI <strong>123456789</strong>
-					</p>
+				<div class="list-group-flush pad pad-left">
+					Estimated time for filing: 90 days	
 				</div>
-				
+			</div>
+
+			<div class="panel bot-10">
+				<div class="panel-heading">
+					<h3 class="panel-title">
+						<i class="icon-pencil edit pull-right"></i>
+						<i class="icon-home"></i> Davenport Medical Clinic
+					</h3>
+				</div>
+				<div class="list-group-flush pad pad-left">
+					Group NPI <strong>199457851</strong>
+					<br />EIN <strong>45-584562</strong>
+				</div>
+			</div>
+
+			<div class="panel bot-10">
+				<div class="panel-heading">
+					<h3 class="panel-title">
+						<i class="icon-pencil edit pull-right"></i>
+						<i class="icon-user-md"></i> Randy Sinnott, DO
+					</h3>
+				</div>
+				<div class="list-group-flush pad pad-left">
+					NPI <strong>123456789</strong>
+				</div>
 			</div>
 		</aside>
 	</div>
 
-	<div class="navbar navbar-fixed-bottom">
+<!-- 	<div class="navbar navbar-fixed-bottom">
 		<div class="container">
 			<div class="row">
 				<div class="col-8">
 					<div class="btn-group btn-group-justified">
 						<a href="#" id="appeal" class="btn btn-large btn-default action"><i class="icon-bolt"></i> Appeal</a>
-						<!-- <a href="#snoozeModal" class="btn btn-large btn-default" data-toggle="modal"><i class="icon-time"></i> Snooze</a> -->
 						<a id="snooze" class="btn btn-large btn-default action"><i class="icon-time"></i> Snooze</a>
 						<a href="#" id="settle" class="btn btn-large btn-default action"><i class="icon-legal"></i> Settle</a>
 						<a href="#" id="reassign" class="btn btn-large btn-default action"><i class="icon-user"></i> Reassign</a>
@@ -203,24 +300,7 @@
 		</div>
 	
 	</div>
-
-	<!-- Modal -->
-	 <div class="modal fade" id="snoozeModal">
-	   <div class="modal-dialog">
-	     <div class="modal-content">
-	       <div class="modal-header">
-	         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-	         <h4 class="modal-title"><i class="icon-time"></i> Snooze</h4>
-	       </div>
-	       <div class="modal-body">
-	       		<form class="form-inline">
-			       	 <!-- <button type="button" id="snooze" data-dismiss="modal" class="btn btn-primary pull-right action">Snooze</button> -->
-			         Remind again in <input type="text" class="form-control" value="5" /> days 
-			    </form>
-	       </div>
-	     </div><!-- /.modal-content -->
-	   </div><!-- /.modal-dialog -->
-	 </div><!-- /.modal -->
+ -->
 
 </div>
 <?php require_once('_footer.php'); ?>
